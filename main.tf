5c5e60c97e3edb6067d2ab65227219b536e9eb91
surendarmsk1 <67820537+surendarmsk1@users.noreply.github.com>
2020-07-22 17:09:50 +0530

    Update and rename main.tf.txt to main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+backend "azurerm" { 
+}
+}
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x.
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+}
+
+# Create a resource group if it doesn't exist
+resource "azurerm_resource_group" "rg" {
+    name     = "resourcegroup-test"
+    location = "westeurppoe"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
